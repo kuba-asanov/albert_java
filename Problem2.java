6bcaefebf1a67daf1c5a52d75039d678c361d17a
Kubanych Asanov <kuba.asanov4@gmail.com>
2024-08-24 00:05:57 +0600

    Problem2

--- Problem2.java
+++ Problem2.java
@@ -1,6 +1,10 @@
 public class Problem2 {
     public static void main(String[] args) {
-    System.out.println("0 1 2 3 4 5 6 7 8 9 10");
+
+         for (int i = 1 ;  i  <= 10 ; i++) {
+            System.out.print(i) ;
+            
+         }
         
     }
 }
