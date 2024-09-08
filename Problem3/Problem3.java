cadcb15aba6a28450ebb2bce8467d67d8bec4709
Kubanych Asanov <kuba.asanov4@gmail.com>
2024-09-08 21:59:25 +0600

    decided

---
+++ Problem3/Problem3.java
@@ -0,0 +1,12 @@
+public class Problem3 {
+    public static void main(String[] args) {
+        
+         for (int i = 10 ; i >= 1 ; i--) {
+             System.out.print(i + " ");
+             
+            
+         }
+        
+    }
+}
+
